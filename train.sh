27d5800ac6ca702aed901f221e495ba53bebf841
佩蔻拉 <61931322+PecholaL@users.noreply.github.com>
2023-08-08 17:43:34 +0800

    ☕️

---
+++ train.sh
@@ -0,0 +1,8 @@
+python3 main.py \
+-c config.yaml \
+-d /Users/pecholalee/Coding/VC/mainVc_data \
+-train_set train \
+-train_index_file train_samples_128.json \
+-store_model_path /Users/pecholalee/Coding/VC/mainVc_data/save/mainVcModel \
+-iters 200000 \
+-summary_steps 500
